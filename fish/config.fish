e7ceaa8b6fe17800fbb6a11f9504428121a1a823
Adarsh <83777943+theCode-Breaker@users.noreply.github.com>
2022-03-14 13:29:40 +0530

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,27 +1,29 @@
-set -U fish_greeting ""
-starship init fish | source
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
+    set -U fish_greeting ""
+    alias fm6000='fm6000 -n -c bright_blue -r'
+    #pacman unlock
+    alias unlock="sudo rm /var/lib/pacman/db.lck"
 
-alias fm6000='fm6000 -n -c bright_blue -r'
-#pacman unlock
-alias unlock="sudo rm /var/lib/pacman/db.lck"
-
-#available free memory
-alias free="free -mt"
+    #available free memory
+    alias free="free -mt"
     
-#Pacman for software managment
-alias search='sudo pacman -Qs'
-alias remove='sudo pacman -R'
-alias install='sudo pacman -S'
-alias linstall='sudo pacman -U '
-alias update='sudo pacman -Syyu'
-alias clrcache='sudo pacman -Scc'
-alias updb='yay && sudo pacman -Sy'
-alias orphans='sudo pacman -Rns $(pacman -Qtdq)'
+    #Pacman for software managment
+    alias search='sudo pacman -Qs'
+    alias remove='sudo pacman -R'
+    alias install='sudo pacman -S'
+    alias linstall='sudo pacman -U '
+    alias update='sudo pacman -Syyu'
+    alias clrcache='sudo pacman -Scc'
+    alias updb='yay && sudo pacman -Sy'
+    alias orphans='sudo pacman -Rns $(pacman -Qtdq)'
     
-#get fastest mirrors in your neighborhood
-alias ram='rate-mirrors --allow-root arch | sudo tee /etc/pacman.d/mirrorlist'
+    #get fastest mirrors in your neighborhood
+    alias ram='rate-mirrors --allow-root arch | sudo tee /etc/pacman.d/mirrorlist'
 
-#grub update
-alias grubup='sudo grub-mkconfig -o /boot/grub/grub.cfg'
+    #grub update
+    alias grubup='sudo grub-mkconfig -o /boot/grub/grub.cfg'
 
-clear && fm6000
+    clear && fm6000
+end
